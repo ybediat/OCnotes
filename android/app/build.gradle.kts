f122c6c56eae00599b85acc35b887163f822e4ec
yannis bediat <y.bediat@gmail.com>
2026-09-19 00:27:53 +0200

    up build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "eu.ocnotes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
         manifestPlaceholders["appAuthRedirectScheme"] = "eu.ocnotes"
 
         // Le .aar de gomobile n'embarque que les ABI passées au bind : cette
